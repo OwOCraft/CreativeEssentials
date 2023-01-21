a79a4a2fcb71a3186c43f03871aee2a95b4e82b7
logzinga <65839350+logzinga@users.noreply.github.com>
2023-01-21 22:01:34 +1100

    added survival mode change

--- src/main/java/com/logzinga/creativeessentials/CreativeEssentials.java
+++ src/main/java/com/logzinga/creativeessentials/CreativeEssentials.java
@@ -1,17 +1,38 @@
 package com.logzinga.creativeessentials;
 
+import org.bukkit.command.BlockCommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CreativeEssentials extends JavaPlugin {
+import java.util.EventListener;
+
+import static org.bukkit.GameMode.SURVIVAL;
+
+public final class CreativeEssentials extends JavaPlugin implements Listener, EventListener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (command.getName().equalsIgnoreCase("survival")) {
+            if (sender instanceof Player) {
+                Player p = (Player)  sender;
+                p.setGameMode(SURVIVAL`);
+            }else if(sender instanceof ConsoleCommandSender) {
+                System.out.println("The console cannot be in survival, you can try this in-game");
+            }else if(sender instanceof BlockCommandSender) {
+                System.out.println("You cannot change your game mode to survival via command block, try in chat");
+            }
+        }
+        return true;
     }
 }
