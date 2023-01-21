2cdb440f117330bb687b49f2ba7e10a03a27e208
logzinga <65839350+logzinga@users.noreply.github.com>
2023-01-21 21:48:56 +1100

    Project Files

---
+++ src/main/java/com/logzinga/creativeessentials/CreativeEssentials.java
@@ -0,0 +1,17 @@
+package com.logzinga.creativeessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CreativeEssentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
